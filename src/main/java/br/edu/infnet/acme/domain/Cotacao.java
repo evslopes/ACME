7e549dc6660485cc78edbfef26682d648e2b8238
Elvis Lopes <evs.lopes@gmail.com>
2022-04-06 19:24:04 -0300

    Implementado classes de dominio

---
+++ src/main/java/br/edu/infnet/acme/domain/Cotacao.java
@@ -0,0 +1,63 @@
+package br.edu.infnet.acme.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Cotacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String origem;
+    private Double valor;
+    private Date dataDeCadastro;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    public Cotacao() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getDataDeCadastro() {
+        return dataDeCadastro;
+    }
+
+    public void setDataDeCadastro(Date dataDeCadastro) {
+        this.dataDeCadastro = dataDeCadastro;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
---
+++ src/main/java/br/edu/infnet/acme/domain/Produto.java
@@ -0,0 +1,63 @@
+package br.edu.infnet.acme.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String nome;
+    private String tipo;
+    private String img;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cotacao_id",  nullable = true)
+    private List<Cotacao> cotacao;
+
+    public Produto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public List<Cotacao> getCotacao() {
+        return cotacao;
+    }
+
+    public void setCotacao(List<Cotacao> cotacao) {
+        this.cotacao = cotacao;
+    }
+}
